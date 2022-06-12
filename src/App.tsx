747fc7f228b91c4926007246df50b0a9875cb740
paulohenriqueresende10 <paulo-henrique_1993@hotmail.com>
2022-06-12 16:24:34 -0300

    feat/mario-jump

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
-
+import clouds from "/src/img/clouds.png"
+import mario from "/src/img/mario.gif"
+import pipe from "/src/img/pipe.png"
 import './App.css'
 
 function App() {
 
   return (
-    <div className="App">
-     <h1> seu cu</h1>
-    </div>
+    <div className="game-board">
+      <img src={clouds} className="clouds"/>
+      <img src={pipe} className="pipe"/>
+      <img src={mario} className="mario"/>
+      <div id="container"></div>
+  </div>
   )
 }
 
