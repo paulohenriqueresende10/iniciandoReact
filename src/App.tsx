51c31355624f4dd290b8c37deeb7df939e55c437
paulohenriqueresende10 <paulo-henrique_1993@hotmail.com>
2022-06-18 19:43:56 -0300

    pokedex

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,10 @@
-import clouds from "./img/clouds.png"
-import mario from "./img/mario.gif"
-import pipe from "./img/pipe.png"
+
 import './App.css'
 
 function App() {
 
   return (
-    <div className="game-board">
-      <img src={clouds} className="clouds"/>
-      <img src={pipe} className="pipe"/>
-      <img src={mario} className="mario"/>
-      <div id="container"></div>
+    <div className="">
   </div>
   )
 }
